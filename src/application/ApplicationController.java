24c81deb7f1158c032b55cdccf50b60b496cf530
dimitrisl <dlolis88@gmail.com>
2016-11-29 16:41:20 +0200

    initiating app controller

--- src/application/ApplicationController.java
+++ src/application/ApplicationController.java
@@ -1,6 +1,15 @@
 package application;
 
+import tools.Request;
+import java.net.Socket;
+import java.util.HashMap;
+
 public class ApplicationController {
+	
+	HashMap<String,Request> request_to_worker ;
+	//create hashtables to contain the worker name (or connection) and the received request from the client
+	
+	
 	//initialize class
 	public ApplicationController()
 	{
