e074195685afeea0fe12751a128765a3bec1e8bc
Dimitrisl <dlolis88@gmail.com>
2016-11-29 23:16:54 +0200

    commenting in Request

--- src/tools/Request.java
+++ src/tools/Request.java
@@ -11,7 +11,7 @@ public class Request {
 	{
 		this.connection_id = slkey;
 		this.question_loc = message;
-		this.answer_json = "";
+		this.answer_json = ""; // the answer from google api
 		this.worker_id = "";
 	}
    
@@ -26,11 +26,11 @@ public class Request {
 	}
 
 	public void setQuestion_loc(String question_loc) {
-		this.question_loc = question_loc;
+		this.question_loc = question_loc; // A concatenated key of the search request by the client
 	}
 
 	public String getWorker_id() {
-		return worker_id;
+		return worker_id; // this has the form of connection_id aka ip address + port
 	}
 
 	public void setWorker_id(String worker_id) {
