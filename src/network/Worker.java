e077c7aac71fcac37ce90ce57e0c464c5cc67316
kvothe88 <kkorovesis88@gmail.com>
2016-11-29 16:30:24 +0200

    worker connectivity initalized

--- src/network/Worker.java
+++ src/network/Worker.java
@@ -1,9 +1,35 @@
 package network;
 
-public class Worker {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
-	public static void main(String[] args) {
+public class TestWorker extends Thread {
+	
+	TestWorker(){
 		
+	}
+	public void run(){
+		Socket requestSocket = null;
+		try {
+			requestSocket = new Socket("127.0.0.1", 4321);//Request socket from Server
+			String message=new String("Hi");
+			DataOutputStream d=new DataOutputStream(requestSocket.getOutputStream());//Send message to SocketListener
+			d.writeInt(message.getBytes().length);
+			d.write(message.getBytes());
+			d.flush();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String[] args) {
+		new TestWorker().start();
 
 	}
 
