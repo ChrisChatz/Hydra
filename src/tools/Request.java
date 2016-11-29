9672df28e89aaa857f7dfe257e30eba2b72695be
Dimitrisl <dlolis88@gmail.com>
2016-11-29 23:10:58 +0200

    created getters and setters for Request

--- src/tools/Request.java
+++ src/tools/Request.java
@@ -19,4 +19,38 @@ public class Request {
 		//to be done with json 
 		return this.toString();
 	}
+
+	public String getConnection_id() {
+		return connection_id;
+	}
+
+	public void setConnection_id(String connection_id) {
+		this.connection_id = connection_id;
+	}
+
+	public String getAnswer_json() {
+		return answer_json;
+	}
+
+	public void setAnswer_json(String answer_json) {
+		this.answer_json = answer_json;
+	}
+
+	public String getQuestion_loc() {
+		return question_loc;
+	}
+
+	public void setQuestion_loc(String question_loc) {
+		this.question_loc = question_loc;
+	}
+
+	public String getWorker_id() {
+		return worker_id;
+	}
+
+	public void setWorker_id(String worker_id) {
+		this.worker_id = worker_id;
+	}
+	
+	
 }
