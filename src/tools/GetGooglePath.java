a179df5cf9e5a889493af6a42c596ed9cdb9875f
Dimitrisl <dlolis88@gmail.com>
2017-02-10 03:21:17 +0200

    simple get from google

--- src/tools/GetGooglePath.java
+++ src/tools/GetGooglePath.java
@@ -1,56 +1,41 @@
 package tools;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URL;
-import java.nio.charset.Charset;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import sun.applet.Main;
+import java.net.*;
+import java.io.*;
 
 public class GetGooglePath {
 	
-	  private static String readAll(Reader rd) throws IOException {
-		    StringBuilder sb = new StringBuilder();
-		    int cp;
-		    while ((cp = rd.read()) != -1) {
-		      sb.append((char) cp);
-		    }
-		    return sb.toString();
-		  }
-	  
-	  // 
-	  public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
-		    InputStream is = new URL(url).openStream();
-		    try {
-		      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-		      String jsonText = readAll(rd);
-		      JSONObject json = new JSONObject(jsonText);
-		      return json;
-		    } finally {
-		      is.close();
-		    }
-		  }
-	  
-	  // This method takes two arguments (source, destination) and returns the path from Google Maps API.
-	  public static JSONObject jsonObject(String start, String finish) throws IOException, JSONException{
+	private static String getUrlContents(String theUrl)
+	  {
+	    StringBuilder content = new StringBuilder();
+	    try
+	    {
 
-		JSONObject json = readJsonFromUrl("https://maps.googleapis.com/maps/api/directions/json?sensor=false&origin=%22+"+start+"+%22&destination=%22+"+finish);
-
-		return json;
-		  
-		  
+	      URL url = new URL(theUrl);
+	      URLConnection urlConnection = url.openConnection();
+	      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+	      String line;
+	      while ((line = bufferedReader.readLine()) != null)
+	      {
+	        content.append(line + "\n");
+	      }
+	      bufferedReader.close();
+	    }
+	    catch(Exception e)
+	    {
+	      e.printStackTrace();
+	    }
+	    return content.toString();
 	  }
-	  // Main method for test only
-	  public static void main(String[] args) throws IOException, JSONException {
-		
-		  JSONObject json = jsonObject("Pikermi", "Faliro");
-		  System.out.println(json.toString());
-	}
 
+	public static String getlink(String start,String finish) throws IOException
+	{
+		String output  = getUrlContents("https://maps.googleapis.com/maps/api/directions/json?sensor=false&origin=%22+"+start+"+%22&destination=%22+"+finish);
+		return output;
+	}
+	
+	public static void main (String[] args) throws IOException
+	{
+		String answer = GetGooglePath.getlink("pikermi", "palini");
+		System.out.println(answer);
+	}
 }
